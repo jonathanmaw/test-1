dc69784767f951cc74eec621c5b45346e636dc2b
Domabrown <dominic.brown@codethink.co.uk>
2017-03-01 13:21:40 +0000

    added errors

--- numberadd.c
+++ numberadd.c
@@ -1,22 +1,28 @@
 #include<stdio.h>
 int main(int argc, char *argv[])
 {
-    int a, sum = 0;
-
-    FILE *filein;
-    filein = fopen(argv[1], "r"); /* opens and reads file */
-      if(argc != 2)
+  int a, sum = 0;
+  
+  if(argc != 2) /* if argc doesnt have atlest 2 arguments not a file */
     {
-        perror("Error: Please enter valid file name: \n"); /* prints error if not a valid file */
+      printf("Error: Please enter valid file name \n"); 
+      return 1;
     }
-    
-    while(fscanf(filein, "%d", &a) == 1)
+  FILE *filein;
+  filein = fopen(argv[1], "r"); /* opens and reads file */
+  
+  if(filein == NULL)
     {
-        sum += a;      /* adds sum of numbers */
+      printf("Error: File does not exist \n");
+      return 1;
+    }
+  while(fscanf(filein, "%d", &a) == 1)
+    {
+      sum += a;      /* adds sum of numbers */
     }
 
-    fclose(filein);
+  fclose(filein);
    
-    printf("Number total from file: %d\n", sum);
-    return 0; 
+  printf("Number total from file: %d\n", sum);
+  return 0; 
 }
