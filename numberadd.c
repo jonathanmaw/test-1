2b7e934b53c49880e28eea8fb31b82a309d206a9
Jonathan Maw <jonathan.maw@codethink.co.uk>
2017-03-23 11:13:13 +0000

    numberadd.c: Amend whitespace to more consistent coding style
    At line 5:
    * Removed the space between function name and braces. Some styles
      mandate this, but the one you were using before didn't, so I assume
      it's in error.
    * Moved the asterisk (between that and your original commit, you went
      from "FILE *" to "FILE*". Most styles recommend the former, because
      "FILE *a, *b"; is valid c, and would look confusing with "FILE*".
    * Also removed the double-space you had.
    
    At line 7:
    * "FILE*" vs. "FILE *", as explained above.
    
    At line 16:
    * Added a blank line between variable declaration and actual files.
      I don't think it's mandated, but you did it before, and it makes
      things easier to read.
    
    At line 22:
    * Removed space between function name and braces.
    
    At line 25:
    * Added a blank line between functions. Makes it easier to read.
    
    At line 27:
    * Removed a stray space.

--- numberadd.c
+++ numberadd.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int read_numb (const char*  file_name)
+int read_numb(const char *file_name)
 {
-  FILE* filein;
+  FILE *filein;
   filein = fopen(file_name, "r"); /* opens file */
   int i = 0;
   int sum = 0;
+
   if(filein == NULL)
     {
       perror("Error: \n");
@@ -19,11 +20,12 @@ int read_numb (const char*  file_name)
       sum += i; /* adds sum of numbers */
     }
 
-  fclose (filein);
+  fclose(filein);
   return sum;
 }
+
 int main(int argc, char *argv[])
- {
+{
   int sum = read_numb(argv[1]);
   printf("Number total from file: %d\n", sum);
   return 0;
