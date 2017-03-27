534de09549ac7dd09397fd7d7b93f878c1352834
Jonathan Maw <jonathan.maw@codethink.co.uk>
2017-03-27 16:50:38 +0100

    Remove trailing whitespace

--- numberadd.c
+++ numberadd.c
@@ -8,18 +8,18 @@ int read_numb(const char *file_name, const int alternate)
 	filein = fopen(file_name, "r"); /* opens file */
 	int i = 0;
 	int sum = 0;
-	
+
 	if (filein == NULL)
 	{
 		perror("Error reading file");
 		exit(1);
 	}
-	
-	
+
+
 	while (fscanf(filein, "%d", &i) == 1)
 	{
 		sum += i; /* adds sum of numbers */
-		
+
 		if (fscanf(filein, "%d", &i) != 1)
 		break;
 
@@ -27,17 +27,17 @@ int read_numb(const char *file_name, const int alternate)
 		{
 			sum -= i;
 		}
-		
+
 		else
 		{
 			sum += i;
 		}
-		
+
 	}
-	
+
 	fclose(filein);
 	return sum;
-	
+
 }
 
 int main(int argc, char *argv[])
@@ -45,21 +45,21 @@ int main(int argc, char *argv[])
 	int alternate = 0;
 	int sum = 0;
 	int opt;
-	
+
 	while ((opt = getopt (argc, argv, "a")) != -1)
 	{
 		if (opt == 'a')
 		{
 			alternate = 1;
 		}
-		
+
 		else
 		{
 			fprintf(stderr, "Usage: %s [-a] file\n", argv[0]);
 			exit(EXIT_FAILURE);
 		}
 	}
-	
+
 	sum = read_numb(argv[optind], alternate);
 	printf("Number total from file: %d\n", sum);
 	return 0;
