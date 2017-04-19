5aa108edbb155f1d5c64e482eab5a556c6494c8c
Domabrown <dominic.brown@codethink.co.uk>
2017-04-19 16:28:02 +0100

    added long line support

--- replace.c
+++ replace.c
@@ -2,11 +2,29 @@
 #include <string.h>
 #include <stdlib.h>
 
+char* readinput(FILE *filein)
+{
+#define CHUNK 100
+   char* input = NULL;
+   char tempbuf[CHUNK];
+   size_t inputlen = 0, templen = 0;
+   do {
+       if(fgets(tempbuf, CHUNK, filein) == NULL)
+	 break;
+       templen = strlen(tempbuf);
+       inputlen += templen;
+       input = realloc(input, inputlen+1);
+       strcat(input, tempbuf);
+    } while (templen==CHUNK-1 && tempbuf[CHUNK-2]!='\n');
+    return input;
+}
+
+
 int main(int argc, char *argv[])
 {
   int c, d;
   FILE *filein, *filew;
-  char replace [100], *start, *end, temp;
+  char *replace, *start, *end, temp;
 
 
     filein = fopen(argv[1], "r");
@@ -25,7 +43,7 @@ int main(int argc, char *argv[])
 
       }
 
-    while(fgets(replace, 100, filein) != NULL)
+    while((replace = readinput(filein)) != NULL)
       {
 	start = replace;
 	end = start + strlen(replace) - 2;
@@ -47,7 +65,7 @@ int main(int argc, char *argv[])
 
     fclose(filew);
     fclose(filein);
-    printf("%s\n", replace);
+    
 
     return 0;
 
